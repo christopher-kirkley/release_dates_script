98320cbbd2abbb09d40426439b3d2a4f8c4adad9
Christopher Kirkley <ckirkley@gmail.com>
2021-01-14 22:57:18 -0800

    Can add arguments to write to emacs.

--- release.py
+++ release.py
@@ -1,34 +1,48 @@
 import numpy as np
 import pandas as pd
 import pyperclip
+import sys
+from datetime import datetime
 
-release_date = input('Choose a release date DD/MM/YY: ')
+def main():
+    release_date = input('Choose a release date DD/MM/YY: ')
 
-df = pd.read_csv('release_dates.csv', index_col='Release Date')
+    df = pd.read_csv('release_dates.csv', index_col='Release Date')
 
-try:
-    dates_df = df.loc[release_date]
-    dates = dates_df.to_dict()
+    try:
+        dates_df = df.loc[release_date]
+        dates = dates_df.to_dict()
 
-    output = ""
+        output = ""
 
-    for key, value in dates.items():
-        print(f'{key}: {value}')
-        output = output + f"{key}: {value}\n"
-    
-    pyperclip.copy(output)
+        for key, value in dates.items():
+            output = output + f"{key}: {value}\n"
+        
+        pyperclip.copy(output)
+        return dates
 
+    except KeyError:
+        print('Invalid date')
 
-except KeyError:
-    print('Invalid date')
+    return dates
 
 # check that is not empty
 
-def write_to_emacs(dates):
-    with open('testproj.org', 'a') as f:
+def write_to_emacs(dates, filename):
+    with open(filename, 'a') as f:
         f.write('\n* TIMELINE\n')
         for item, date in dates.items():
-            f.write(f'** TODO {item}\n')
-            f.write(f'  DEADLINE: <2021-05-21 Tue>\n')
+            try:
+                date_object = datetime.strptime(date, '%m/%d/%y')
+                date = date_object.strftime('%Y-%m-%d %a')
+
+                f.write(f'** TODO {item}\n')
+                f.write(f'  DEADLINE: <{date}>\n')
+            except:
+                pass
         
-write_to_emacs(dates)
+if __name__ == "__main__":
+    dates = main()
+    if sys.argv[1] == '-e':
+        write_to_emacs(dates, sys.argv[2])
+
